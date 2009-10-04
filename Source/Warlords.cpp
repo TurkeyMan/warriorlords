a9475dbc7d7b13cef9b4d555853fe0a300645f99
turkeyman <turkeyman@f82a8f4f-13e0-4542-855c-6df575116ada>
2009-10-04 15:32:52 +0000

    Inverted the mountain-hill ring.
    git-svn-id: http://localhost/svn/warlords@106 f82a8f4f-13e0-4542-855c-6df575116ada

--- Source/Warlords.cpp
+++ Source/Warlords.cpp
@@ -32,7 +32,7 @@ void Game_Init()
 	mountData.pPath = MFFile_SystemPath("Data/");
 	MFFileSystem_Mount(hNative, &mountData);
 
-#if 0
+#if 1
 	if(MFFileSystem_Exists("Map.ini"))
 		pEditor = new Editor("Map");
 	else
