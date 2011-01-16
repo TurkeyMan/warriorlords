f419dc00c63ab9700df1ee9e3bf9cf3e650cba38
turkeyman <turkeyman@f82a8f4f-13e0-4542-855c-6df575116ada>
2011-01-16 04:32:04 +0000

    - Premake config files.
    git-svn-id: http://localhost/svn/warlords@572 f82a8f4f-13e0-4542-855c-6df575116ada

---
+++ Project/premake4.lua
@@ -0,0 +1,54 @@
+solution "Warlords"
+	configurations { "Debug", "DebugOpt", "Release", "Retail" }
+--	platforms { "Native", "x32", "x64", "Xbox360", "PS3" }
+	platforms { "Native", "x32", "x64" }
+
+	-- include the fuji project...
+	dofile  "../../Fuji/Project/fujiproj.lua"
+
+	project "Warlords"
+		kind "WindowedApp"
+		language "C++"
+		files { "../Source/**.h", "../Source/**.cpp" }
+
+		includedirs { "../Source/" }
+		objdir "../Build/"
+		targetdir "../"
+
+		flags { "StaticRuntime", "NoExceptions", "NoRTTI" }
+
+		pchheader "Warlords.h"
+		pchsource "Warlords.cpp"
+
+		links { "Fuji" }
+
+		configuration "Debug"
+			defines { "DEBUG", "_DEBUG" }
+			flags { "Symbols" }
+			targetsuffix "_Debug"
+
+		configuration "DebugOpt"
+			defines { "DEBUG", "_DEBUG" }
+			flags { "Symbols", "OptimizeSpeed" }
+			targetsuffix "_DebugOpt"
+
+		configuration "Release"
+			defines { "NDEBUG", "_RELEASE" }
+			flags { "OptimizeSpeed" }
+			targetsuffix "_Release"
+
+		configuration "Retail"
+			defines { "NDEBUG", "_RETAIL" }
+			flags { "OptimizeSpeed" }
+			targetsuffix "_Retail"
+
+
+		-- need to filter the proper set of macros for various visual studio platforms
+		configuration { "windows or Xbox360", "not PS3" }
+			defines { "WIN32" }
+		configuration { "windows", "not Xbox360", "not PS3" }
+			defines { "_WINDOWS" }
+		configuration "Xbox360"
+			defines { "_XBOX" }
+		configuration "PS3"
+			defines { "_PS3" }
