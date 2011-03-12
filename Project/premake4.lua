9db987cffe0d4d89548caa60109ce077619cea16
turkeyman <turkeyman@f82a8f4f-13e0-4542-855c-6df575116ada>
2011-03-12 08:26:41 +0000

    - Updated the WarriorLords premake script.
    git-svn-id: http://localhost/svn/warlords@579 f82a8f4f-13e0-4542-855c-6df575116ada

--- Project/premake4.lua
+++ Project/premake4.lua
@@ -1,6 +1,7 @@
 solution "Warlords"
 	configurations { "Debug", "DebugOpt", "Release", "Retail" }
 --	platforms { "Native", "x32", "x64", "Xbox360", "PS3" }
+--	platforms { "Native", "Xbox" }
 	platforms { "Native", "x32", "x64" }
 
 	-- include the fuji project...
@@ -23,77 +24,4 @@ solution "Warlords"
 
 		links { "Fuji" }
 
-		configuration "Debug"
-			defines { "DEBUG", "_DEBUG" }
-			flags { "Symbols" }
-			targetsuffix "_Debug"
-
-		configuration "DebugOpt"
-			defines { "DEBUG", "_DEBUG" }
-			flags { "Symbols", "OptimizeSpeed" }
-			targetsuffix "_DebugOpt"
-
-		configuration "Release"
-			defines { "NDEBUG", "_RELEASE" }
-			flags { "OptimizeSpeed" }
-			targetsuffix "_Release"
-
-		configuration "Retail"
-			defines { "NDEBUG", "_RETAIL" }
-			flags { "OptimizeSpeed" }
-			targetsuffix "_Retail"
-
-
-		-- need to filter the proper set of macros for various visual studio platforms
-		configuration { "windows or Xbox360", "not PS3" }
-			defines { "WIN32" }
-		configuration { "windows", "not Xbox360", "not PS3" }
-			defines { "_WINDOWS" }
-		configuration "Xbox360"
-			defines { "_XBOX" }
-		configuration "PS3"
-			defines { "_PS3" }
-
-
-		-- platform specific config --
-
-		-- Linux --
-		configuration { "linux" }
-			defines { "_LINUX" }
-			links { "c", "m", "stdc++", "pthread", "GL", "GLU", "Xxf86vm", "X11", "ogg", "vorbis", "vorbisfile", "asound", "portaudio" }
-			links { "z", "mad", "png", "json" }
-
-		-- OSX --
-		configuration { "macosx" }
-			defines { "_OSX" }
-			links { "c", "m", "stdc++", "pthread", "GL", "GLU", "Xxf86vm", "X11", "ogg", "vorbis", "vorbisfile" }
-			links { "z", "mad", "png", "json" }
-
-		-- Windows --
-		configuration { "windows", "not Xbox360", "not PS3" }
-			defines { "WIN32", "_WINDOWS" }
-			links { "ogg_static", "vorbisfile_static", "mad", "json" }
-
-			-- Windows 32 --
-			configuration { "windows", "x32 or native", "not Xbox360", "not PS3" }
-				libdirs { "../Middleware/vorbis/lib/x86", "../Middleware/lglcd/lib/x86" }
-				
-			-- Windows 64 --
-			configuration { "windows", "x64", "not Xbox360", "not PS3" }
-				libdirs { "../Middleware/vorbis/lib/x64", "../Middleware/lglcd/lib/x64" }
-
-		-- XBox --
-		configuration "Xbox"
-			defines { "WIN32", "_XBOX" }
-			libdirs { "../Middleware/vorbis/lib/xbox" }
-			links { "mad", "json" }
-
-		-- XBox 360 --
-		configuration "Xbox360"
-			defines { "WIN32", "_XBOX" }
-			links { "mad", "json" }
-
-		-- Playstation 3 --
-		configuration "PS3"
-			defines { "_PS3" }
-			links { "mad", "json" }
+		dofile "../../Fuji/Project/fujiconfig.lua"
