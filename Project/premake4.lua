29ccd6b9a0b7cb2862032bea3cd85e0be3148ff4
turkeyman <turkeyman@f82a8f4f-13e0-4542-855c-6df575116ada>
2011-06-12 21:36:11 +0000

    - Added initial support for Android!
    git-svn-id: http://localhost/svn/warlords@628 f82a8f4f-13e0-4542-855c-6df575116ada

--- Project/premake4.lua
+++ Project/premake4.lua
@@ -2,7 +2,7 @@ solution "Warlords"
 	configurations { "Debug", "DebugOpt", "Release", "Retail" }
 --	platforms { "Native", "x32", "x64", "Xbox360", "PS3" }
 --	platforms { "Native", "Xbox" }
-	platforms { "Native", "x32", "x64" }
+	platforms { "Native", "x32", "x64", "Android" }
 
 	-- include the fuji project...
 	dofile  "../../Fuji/Fuji/Private/Project/fujiproj.lua"
